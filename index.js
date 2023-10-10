407f6310e7b86649961e4d72948f8dc5d2bbeaf8
AT2112 <andry.timoshenko@gmail.com>
2023-10-10 14:34:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  const file = req.file;
+
+  console.log(file)
+
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  } 
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
